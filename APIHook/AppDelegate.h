e7a368131428cf89a11be55a4c2131fc55676662
Jason Humphries <humphriesj@gmail.com>
2014-04-05 07:12:10 -0400

    Initial Commit

---
+++ APIHook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APIHook
+//
+//  Created by Jason Humphries on 4/5/14.
+//  Copyright (c) 2014 Jason Humphries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ APIHook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  APIHook
+//
+//  Created by Jason Humphries on 4/5/14.
+//  Copyright (c) 2014 Jason Humphries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
